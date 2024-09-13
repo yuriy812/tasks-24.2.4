31cf12da76479c4531b79dac1be526e37f85abc1
yuriy <sitecadilac@mail.ru>
2024-09-13 12:48:44 +0300

    initial-1

---
+++ tests/operations_test.py
@@ -0,0 +1,20 @@
+import pytest
+from app.calc import Calculator
+
+
+class TestOperation:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+    def test_subtracting_success(self):
+        assert self.calc.adding(self, 1, 1) == 3
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+
+    def tearown(self):
+        print("Выполнение метода Teardown")
