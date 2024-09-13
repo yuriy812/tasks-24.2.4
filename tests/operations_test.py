57878beb1eac843c75d87562cc4a5a1688c823fa
yuriy <sitecadilac@mail.ru>
2024-09-13 12:56:05 +0300

    test 1

--- tests/operations_test.py
+++ tests/operations_test.py
@@ -3,18 +3,18 @@ from app.calc import Calculator
 
 
 class TestOperation:
-    def setup(self):
-        self.calc = Calculator
+    def setup_method(self):
+        self.calc = Calculator()
 
     def test_adding_success(self):
-        assert self.calc.adding(self, 1, 1) == 2
+        assert self.calc.adding(1, 1) == 2
 
     def test_subtracting_success(self):
-        assert self.calc.adding(self, 1, 1) == 3
+        assert self.calc.subtracting(3, 1) == 2
 
     def test_zero_division(self):
         with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 1, 0)
+            self.calc.division(1, 0)
 
-    def tearown(self):
+    def teardown_method(self):
         print("Выполнение метода Teardown")
